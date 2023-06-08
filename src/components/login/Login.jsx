e833a413ad3f1e27072f6db8371091eb7d9be659
unknown <johnmsn22@gmail.com>
2023-06-08 07:06:35 +0100

    login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,19 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.scss";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({ email: "", password: "" });
+
+    let hasError = false;
+
+    if (!email) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is required.",
+      }));
+      hasError = true;
+    }
+
+    if (!password) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Password is required.",
+      }));
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
+
+    try {
+      setIsLoading(true)
+      // Sign in with email and password
+      await signInWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+    } catch (error) {
+      // Handle login errors
+      if (error.code === "auth/user-not-found") {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          email: "User not found. Please check your email.",
+        }));
+      } else if (error.code === "auth/wrong-password") {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          password: "Incorrect password. Please try again.",
+        }));
+      } else {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          email: error.message,
+        }));
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="login__">
       <div className="login__wrapper">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input__container">
-            <input type="text" placeholder="Email" />
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && (
+              <span className="error__message">{errors.email}</span>
+            )}
           </div>
           <div className="input__container">
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && (
+              <span className="error__message">{errors.password}</span>
+            )}
           </div>
           <div className="btn__container">
-            <button type="submit">Log In</button>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Logging in..." : "Log In"}
+            </button>
           </div>
         </form>
       </div>
--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -4,16 +4,78 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const SignUp = () => {
   const [fullName, setFullName] = useState("");
-  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false); // Added loading state
+
+  const validateEmail = (email) => {
+    // Email validation using regex pattern
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return false;
+    }
+
+    // Domain checking
+    const allowedDomains = [
+      "gmail.com",
+      "ymail.com",
+      "yahoo.com",
+      "example.com",
+    ]; // Add your allowed domains here
+    const domain = email.split("@")[1];
+
+    return allowedDomains.includes(domain);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(null);
+    setErrors({ fullName: "", email: "", password: "" });
+
+    let hasError = false;
+
+    if (!fullName) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        fullName: "Full Name is required.",
+      }));
+      hasError = true;
+    }
+
+    if (!validateEmail(email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Please enter a valid email address.",
+      }));
+      hasError = true;
+    }
+
+    if (!password) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Password is required.",
+      }));
+      hasError = true;
+    } else if (password.length < 6) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Password should be at least 6 characters long.",
+      }));
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
 
     try {
+      setIsLoading(true); // Start loading
+
       // Create user account with email and password
       const userCredential = await createUserWithEmailAndPassword(
         auth,
@@ -29,9 +91,21 @@ const SignUp = () => {
         displayName: fullName,
       });
 
+      setUser(newUser);
+      setIsLoading(false); // Stop loading
+
       // Additional logic after successful sign-up (e.g., redirect, show success message)
     } catch (error) {
-      setError(error.message);
+      console.log(error.code);
+      if (error.code === "auth/email-already-in-use") {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          email: "Email is already in use.",
+        }));
+      } else {
+        setErrors((prevErrors) => ({ ...prevErrors, password: error.message }));
+      }
+      setIsLoading(false); // Stop loading
     }
   };
 
@@ -46,14 +120,9 @@ const SignUp = () => {
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
             />
-          </div>
-          <div className="input__container">
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            {errors.fullName && (
+              <span className="error__message">{errors.fullName}</span>
+            )}
           </div>
           <div className="input__container">
             <input
@@ -62,6 +131,9 @@ const SignUp = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.email && (
+              <span className="error__message">{errors.email}</span>
+            )}
           </div>
           <div className="input__container">
             <input
@@ -70,12 +142,19 @@ const SignUp = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+            {errors.password && (
+              <span className="error__message">{errors.password}</span>
+            )}
           </div>
           <div className="btn__container">
-            <button type="submit">Sign Up</button>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Signing up..." : "Sign Up"}
+            </button>
           </div>
         </form>
-        {error && <div className="error__message">{error}</div>}
+        {user && (
+          <span className="success__message">Signed up successfully!</span>
+        )}
       </div>
     </div>
   );
